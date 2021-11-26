16270a0f03a575bdda17e97666ddafe93e5dcd02
timofeyh <timofeyhartanovich@gmail.com>
2021-11-26 05:01:35 -0500

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const quiz = require('./questions.json');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(express.static("./client/build"));
 app.use(express.json());
